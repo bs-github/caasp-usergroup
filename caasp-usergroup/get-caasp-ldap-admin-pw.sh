b071c1649f225876963499f6f7f05e82838ea488
Birger Schmidt <bschmidt@suse.de>
2017-11-19 17:33:22 +0100

    use the CAASP_ADMIN_NODE var in get-caasp-ldap-admin-pw.sh

--- caasp-usergroup/get-caasp-ldap-admin-pw.sh
+++ caasp-usergroup/get-caasp-ldap-admin-pw.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-OPENLDAP_CONTAINER_ID=$(ssh root@admin docker ps | grep openldap | awk '{ print $1 }')
+if [ -z ${CAASP_ADMIN_NODE} ]
+then
+  echo
+  echo "ERROR: You must supply the address for the CaaS Platform Admin node as CAASP_ADMIN_NODE=<hostname>."
+  exit 1
+fi
+OPENLDAP_CONTAINER_ID=$(ssh root@${CAASP_ADMIN_NODE} docker ps | grep openldap | awk '{ print $1 }')
 
-#ssh -qt root@admin docker exec -i '$(docker ps | grep openldap | awk '{ print $1 }')' cat /var/lib/misc/infra-secrets/openldap-password
-ssh -qt root@admin docker exec -i ${OPENLDAP_CONTAINER_ID} cat /var/lib/misc/infra-secrets/openldap-password
+#ssh -qt root@${CAASP_ADMIN_NODE} docker exec -i '$(docker ps | grep openldap | awk '{ print $1 }')' cat /var/lib/misc/infra-secrets/openldap-password
+ssh -qt root@${CAASP_ADMIN_NODE} docker exec -i ${OPENLDAP_CONTAINER_ID} cat /var/lib/misc/infra-secrets/openldap-password
